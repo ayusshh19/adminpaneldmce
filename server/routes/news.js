83214883c5e0318796b0db921b85a392ef447360
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2023-08-19 09:19:47 +0530

    final updates on news section

--- server/routes/news.js
+++ server/routes/news.js
@@ -2,7 +2,8 @@ import express from 'express'
 import con from '../connection/dbconnect.js'
 import bodyParser from 'body-parser'
 import multer from 'multer'
-const upload = multer({ dest: './uploads/' })
+var storage = multer.memoryStorage()
+var upload = multer({ storage: storage })
 const router = express.Router()
 
 router.use(bodyParser.urlencoded({
@@ -34,6 +35,7 @@ router.post("/api/news/addnews", (req, res) => {
     const file =reqbody.file?reqbody.file:null
     const dept= reqbody.dept
     const sql = 'INSERT INTO `dmcesite2021_news`(`news`, `link`,`description`,`expiry`,`fileLink`,`dept`,`important`) VALUES( ?,?,?,?,?,?,?)'
+    console.log(file) 
     con.query(sql, [title, link, desc, imp, expiry, file, dept], (err, result) => {
         if (err) {
             res.json(err)
@@ -45,8 +47,36 @@ router.post("/api/news/addnews", (req, res) => {
     });
 });
 
+router.put("/api/news/archievenews", (req, res) => {
+    const reqbody = req.body
+    console.log(reqbody)
+    const postid=reqbody.id
+    const sql = 'UPDATE `dmcesite2021_news` SET `archived` = ? WHERE `sr` = ? '
+    con.query(sql, [1,postid], (err, result) => {
+        if (err) {
+            res.json(err)
+        } else {
+            console.log("1 record successfully updated into db");
+            res.json({"msg":result})
+        }
 
+    });
+});
 
+router.delete("/api/news/deletenews", (req, res) => {
+    const reqbody = req.body
+    const postid=reqbody.id
+    const sql = 'DELETE FROM `dmcesite2021_news` WHERE `sr` = ?'
+    con.query(sql, [postid], (err, result) => {
+        if (err) {
+            res.json(err)
+        } else {
+            console.log("1 record successfully deleted from db");
+            res.json({"msg":result})
+        }
+
+    });
+});
 
 
 export default router;
